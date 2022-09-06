d415977c86f8e36a9294f7ac6ad8f065107949eb
vanesssa79 <vanesssa79@hotmail.com>
2022-09-06 07:15:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Cliente cliente1 = new Cliente("Vicente", "656 74 76 84", "C/ Pers", "llomo@kof.com",
+        "Sí");
+Console.WriteLine("Nombre: " + cliente1.Nombre);
+Console.WriteLine("Dirección: " + cliente1.Direccion );
+Console.WriteLine("Teléfono: " + cliente1.Telefono);
+Console.WriteLine("Email: " + cliente1.Email);
+Console.WriteLine("Cliente nuevo: " + cliente1.NewCliente);
+public struct Cliente
+{
+   public Cliente(string nombre, string telefono, string direccion, string email,
+        string newCliente)
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+        NewCliente = newCliente;
+
+    }
+
+    public string Nombre{get;}
+    public string Telefono { get; }
+    public string Direccion{ get; }
+    public string Email { get; }
+    public string NewCliente { get; }
+
+}
+
